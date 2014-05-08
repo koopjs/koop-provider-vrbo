26fa876030102f3a5619cc563a74dfeaa2c47489
chelm <christopher.helm@gmail.com>
2014-05-08 12:09:56 -0600

    tests

--- test/routes-test.js
+++ test/routes-test.js
@@ -24,5 +24,49 @@ describe('Koop Routes', function(){
       });
     });
 
+    describe('/vrbo/-116.997/34.225/-116.785/34.265', function() {
+      it('should return 200', function(done) {
+        request(koop)
+          .get('/vrbo/-116.997/34.225/-116.785/34.265')
+          .end(function(err, res){
+            res.should.have.status(200);
+            done();
+          });
+      });
+    });
+
+    describe('/vrbo/-116.997/34.225/-116.785/34.265/FeatureServer', function() {
+      it('should return 200', function(done) {
+        request(koop)
+          .get('/vrbo/-116.997/34.225/-116.785/34.265/FeatureServer')
+          .end(function(err, res){
+            res.should.have.status(200);
+            done();
+          });
+      });
+    });
+
+    describe('/vrbo/-116.997/34.225/-116.785/34.265/FeatureServer/0', function() {
+      it('should return 200', function(done) {
+        request(koop)
+          .get('/vrbo/-116.997/34.225/-116.785/34.265/FeatureServer/0')
+          .end(function(err, res){
+            res.should.have.status(200);
+            done();
+          });
+      });
+    });
+
+    describe('/vrbo/-116.997/34.225/-116.785/34.265/FeatureServer/0/query', function() {
+      it('should return 200', function(done) {
+        request(koop)
+          .get('/vrbo/-116.997/34.225/-116.785/34.265/FeatureServer/0/query')
+          .end(function(err, res){
+            res.should.have.status(200);
+            done();
+          });
+      });
+    });
+
 });
 
